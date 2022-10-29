dc18aa6caedb97af1c25a9f716cb15f7142ad5f3
jailton-dantas <jajadantas8@hotmail.com>
2022-10-29 16:01:53 -0300

    comitProjeto

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,9 @@
+describe("Suite de teste de exemplo com NodeJS", () => {
+    it("Deve demonostrar o uso do Jasmine com Node.js", () => {
+        expect(true).toEqual(true);
+    });
+
+    it("Deve demonstrar o uso do karma em modo escuta", () => {
+        expect(1).toEqual(1);
+    })
+});
